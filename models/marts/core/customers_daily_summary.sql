2381e8483631f738d3e1ae6c1d86b62d851e8235
Just Sala <justmaister@gmail.com>
2022-04-11 08:26:24 +0000

    delete github package

--- models/marts/core/customers_daily_summary.sql
+++ models/marts/core/customers_daily_summary.sql
@@ -3,6 +3,6 @@ select
     {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as id,
     customer_id,
     order_date,
-    count(*)
+    count(*) as num_orders
 from {{ ref('stg_orders') }}
 group by 1,2,3
